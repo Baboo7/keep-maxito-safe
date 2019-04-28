5599b64869e7ac94c611097cda4357cbc24b1b58
Baboo7 <dbsharp404@gmail.com>
2019-04-28 12:49:08 +0200

    Rework alert page

--- front/src/pages/Alert/Alert.component.tsx
+++ front/src/pages/Alert/Alert.component.tsx
@@ -1,8 +1,35 @@
 import React, { PureComponent, ReactNode } from "react";
+import styled from "styled-components";
+
 import { Page } from "../../components/Page";
 
-export class Alert extends PureComponent {
+import { EnterMessage } from "./components/EnterMessage";
+
+interface IState {
+  messageUrl: string | null;
+}
+
+export class Alert extends PureComponent<{}, IState> {
+  public state: IState = {
+    messageUrl: null
+  };
+
   public render(): ReactNode {
-    return <Page />;
+    return (
+      <Page>
+        <ContentWrapper>
+          <EnterMessage />
+        </ContentWrapper>
+      </Page>
+    );
   }
 }
+
+export const ContentWrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  max-width: 100vw;
+  align-items: center;
+`;
