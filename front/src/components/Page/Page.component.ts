2d44c681f6cd443dd0eb1d86c513a91d56898ea4
Baboo7 <dbsharp404@gmail.com>
2019-04-28 12:38:44 +0200

    Add background dot

--- front/src/components/Page/Page.component.ts
+++ front/src/components/Page/Page.component.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import dotImage from "../assets/images/dot.png";
+import dotImage from "../../assets/images/dot.png";
 
 export const Page = styled.section`
   display: flex;
