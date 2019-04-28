5fccc2be4bb2b13864c1e0de380080cef8bf46b0
Baboo7 <dbsharp404@gmail.com>
2019-04-28 12:47:42 +0200

    Create button component

---
+++ front/src/components/Button/index.ts
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+import { colors, fonts } from "../../styles";
+
+export const Button = styled.button`
+  padding-left: 16px;
+  padding-right: 16px;
+  box-shadow: 2px 2px 8px ${colors.verylightgray};
+  border: 0;
+  border-bottom: 7px solid ${colors.red};
+  border-radius: 5px;
+  background-color: ${colors.lightred};
+  color: ${colors.white};
+  font-family: ${fonts.text};
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+
+  &:disabled {
+    background-color: ${colors.verylightgray};
+    color: ${colors.lightgray};
+    border-bottom-color: ${colors.lightgray};
+  }
+`;
